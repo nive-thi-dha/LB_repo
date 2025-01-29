6d6f2224c7fc36c5e7bcd818b70b479ee87b4451
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-01-29 15:08:51 +0530

    Update test_table.sql

--- changelog/change/test_table.sql
+++ changelog/change/test_table.sql
@@ -1,9 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<databaseChangeLog
-    xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
-    http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.4.xsd">
+--liquibase formatted sql
+
+-- changeset your_name:1001
 USE SCHEMA SCH;
 CREATE OR REPLACE TABLE TEST_TABLE
 (
